20ff5ade95a5b54c63b7f29d32454ff144ef5172
vosmont <vincent.osmont@gmail.com>
2016-08-18 16:29:24 +0200

    version 0.12.3 hot-fix Fix the version

--- luup files/L_RulesEngine1.lua
+++ luup files/L_RulesEngine1.lua
@@ -22,7 +22,7 @@ end
 
 _NAME = "RulesEngine"
 _DESCRIPTION = "Rules Engine for the Vera with visual editor"
-_VERSION = "0.12.2"
+_VERSION = "0.12.3"
 _AUTHOR = "vosmont"
 
 -- **************************************************
