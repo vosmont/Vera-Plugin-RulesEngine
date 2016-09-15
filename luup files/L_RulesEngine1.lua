a77c3c45435d9d819933266d29d1efb08ce62597
vosmont <nosbox@free.fr>
2016-09-15 08:49:32 +0200

    fix version

--- luup files/L_RulesEngine1.lua
+++ luup files/L_RulesEngine1.lua
@@ -22,7 +22,7 @@ end
 
 _NAME = "RulesEngine"
 _DESCRIPTION = "Rules Engine for the Vera with visual editor"
-_VERSION = "0.15"
+_VERSION = "0.15.1"
 _AUTHOR = "vosmont"
 
 -- **************************************************
